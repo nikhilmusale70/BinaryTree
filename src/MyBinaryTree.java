2306f799e936cfc86f4f07e3e5acb2ec30eb2858
Nikhil Musale <nikhilmusale70@gmail.com>
2021-08-26 17:36:12 +0530

    [Nikhil] - Added bst inseret function

--- src/MyBinaryTree.java
+++ src/MyBinaryTree.java
@@ -1,3 +1,25 @@
 public class MyBinaryTree {
 
+    public Node createNewNode(int key){
+        Node n = new Node();
+        n.key=key;
+        n.leftNode=null;
+        n.rightNode=null;
+
+        return n;
+    }
+
+    public Node insert(Node node, int key){
+        if (node==null){
+            return createNewNode(key);
+        }
+
+        if (key<node.key)
+            node.leftNode = insert(node.leftNode, key);
+        else if (key< node.key)
+            node.rightNode = insert(node.rightNode, key);
+
+
+        return node;
+    }
 }
---
+++ src/MyBinaryTreeRunner.java
@@ -0,0 +1,10 @@
+public class MyBinaryTreeRunner {
+    public static void main(String[] args) {
+        MyBinaryTree bst = new MyBinaryTree();
+        Node root = null;
+        root = bst.insert(root, 56);
+        root = bst.insert(root, 30);
+        root = bst.insert(root, 70);
+
+    }
+}
